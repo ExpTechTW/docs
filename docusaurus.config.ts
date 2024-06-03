4a21a7cc617e7d3c3db04c35f4ce00111356dab4
whes1015 <whes1015@gmail.com>
2024-06-04 05:51:37 +0800

    fix: docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -112,7 +112,7 @@ const config: Config = {
             },
             {
               label: 'CDPS',
-              to: '/docs/cdps/intro',
+              to: '/docs/cdps/start/index',
             },
           ],
         },
