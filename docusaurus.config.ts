86c5b42c92ce7be617348b310d9794b2f5165571
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-06-03 20:54:48 +0800

    chore: baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.exptech.com.tw',
+  url: 'https://exptech.com.tw',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
