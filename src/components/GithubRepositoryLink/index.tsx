5712c5e0f49eabca07302a2d5e10f9db694a59ae
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-06-08 12:27:53 +0800

    i18n(component/GithubRepositoryLink): source code translation

--- src/components/GithubRepositoryLink/index.tsx
+++ src/components/GithubRepositoryLink/index.tsx
@@ -1,6 +1,8 @@
 import styles from "./styles.module.css";
 import GithubIcon from "@site/static/img/github.svg";
 
+import Translate, {translate} from '@docusaurus/Translate';
+
 type GithubRepositoryLinkProps = {
   url: string;
 }
@@ -9,7 +11,13 @@ export default function GithubRepositoryLink({ url }: GithubRepositoryLinkProps)
   return (
     <a className={styles.repolink} href={url}>
       <GithubIcon className={styles.icon} />
-      <div className={styles.name}>原始碼</div>
+      <div className={styles.name}>
+        <Translate
+          id="component.githubRepositoryLink.sourceCode"
+          description="The label of source code link">
+          原始碼
+        </Translate>
+      </div>
     </a>
   )
 }
