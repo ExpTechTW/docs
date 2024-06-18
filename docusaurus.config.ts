eb7196b69ca41904caeb46343eae90c31bac9843
夜夜 <vcde0000@gmail.com>
2024-06-19 02:02:57 +0800

    fix: docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://exptech.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs',
+  baseUrl: '/exptechdocs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
